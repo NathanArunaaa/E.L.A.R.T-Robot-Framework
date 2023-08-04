fe8b61df4afb098ccaf9a8a86a99a6dd4f7d36dd
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-03 20:55:57 -0400

    new ip

--- RF_Data/Transmitter.py
+++ RF_Data/Transmitter.py
@@ -3,33 +3,15 @@ import socket
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the Raspberry Pi's IP address and port
-host = 'RASPBERRY_PI_IP_ADDRESS'  # Replace with the Raspberry Pi's IP address on the access point network
-port = 12345                      # Use the same port number chosen on the Raspberry Pi
-client_socket.connect((host, port))
+# Connect to the Raspberry Pi's access point
+server_address = ('192.168.4.1', 12345)  # Replace 'x.x.x.x' with the IP address of the access point
+client_socket.connect(server_address)
 
 while True:
-    command = input("Enter a command: ")
-    client_socket.send(command.encode())
-    if command.lower() == 'exit':
+    data = client_socket.recv(1024).decode()
+    if not data:
         break
+    # Process the received data/command here
+    print("Received:", data)
 
 client_socket.close()
-
-        
-        
-        
-		
-    
-    
-    
-    
-	
-
-
-
-
-
-
-
- 
