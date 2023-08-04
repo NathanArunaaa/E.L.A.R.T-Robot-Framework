c4c4777594b80a3a00dac63721e0357370c36b46
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-04 15:26:58 -0400

    robot side logic

--- RobotStartup.py
+++ RobotStartup.py
@@ -1,51 +1,13 @@
 import socket
-import RPi.GPIO as GPIO
-import time
-
-# Set up GPIO pins for robot movement
-GPIO.setmode(GPIO.BCM)
-# Replace the following pin numbers with the actual GPIO pins connected to the robot's motors
-motor_a_forward_pin = 17
-motor_a_backward_pin = 18
-motor_b_forward_pin = 22
-motor_b_backward_pin = 23
-
-GPIO.setup(motor_a_forward_pin, GPIO.OUT)
-GPIO.setup(motor_a_backward_pin, GPIO.OUT)
-GPIO.setup(motor_b_forward_pin, GPIO.OUT)
-GPIO.setup(motor_b_backward_pin, GPIO.OUT)
-
-# Function to control the robot's movement
-def move_forward():
-    GPIO.output(motor_a_forward_pin, GPIO.HIGH)
-    GPIO.output(motor_a_backward_pin, GPIO.LOW)
-    GPIO.output(motor_b_forward_pin, GPIO.HIGH)
-    GPIO.output(motor_b_backward_pin, GPIO.LOW)
-
-def move_backward():
-    GPIO.output(motor_a_forward_pin, GPIO.LOW)
-    GPIO.output(motor_a_backward_pin, GPIO.HIGH)
-    GPIO.output(motor_b_forward_pin, GPIO.LOW)
-    GPIO.output(motor_b_backward_pin, GPIO.HIGH)
-
-def turn_left():
-    GPIO.output(motor_a_forward_pin, GPIO.LOW)
-    GPIO.output(motor_a_backward_pin, GPIO.HIGH)
-    GPIO.output(motor_b_forward_pin, GPIO.HIGH)
-    GPIO.output(motor_b_backward_pin, GPIO.LOW)
-
-def turn_right():
-    GPIO.output(motor_a_forward_pin, GPIO.HIGH)
-    GPIO.output(motor_a_backward_pin, GPIO.LOW)
-    GPIO.output(motor_b_forward_pin, GPIO.LOW)
-    GPIO.output(motor_b_backward_pin, GPIO.HIGH)
+import cv2
+import numpy as np
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific IP and port
 host = '0.0.0.0'  # Use 0.0.0.0 to listen on all available interfaces
-port = 87     # Choose a port number for the communication
+port = 12345     # Choose a port number for the communication
 server_socket.bind((host, port))
 
 # Start listening for incoming connections
@@ -55,7 +17,20 @@ print("Waiting for a connection...")
 conn, addr = server_socket.accept()
 print("Connected to:", addr)
 
+# Initialize the camera
+cap = cv2.VideoCapture(0)
+
 while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    # Convert the frame to JPEG format
+    _, encoded_frame = cv2.imencode('.jpg', frame)
+
+    # Send the encoded frame to the client
+    conn.sendall(encoded_frame.tobytes())
+
+    # Receive commands from the client
     data = conn.recv(1024).decode()
     if not data:
         break
@@ -63,23 +38,21 @@ while True:
     # Process the received command and control the robot's movement
     if data == 'forward':
         print("Moving forward")
-        move_forward()
+        # Implement your robot's forward movement logic here
     elif data == 'backward':
         print("Moving backward")
-        move_backward()
+        # Implement your robot's backward movement logic here
     elif data == 'left':
         print("Turning left")
-        turn_left()
+        # Implement your robot's left-turn movement logic here
     elif data == 'right':
         print("Turning right")
-        turn_right()
+        # Implement your robot's right-turn movement logic here
     else:
         print("Invalid command")
 
-    # Add a small delay to control the duration of the movement
-    time.sleep(0.5)
-
-# Clean up GPIO pins
-GPIO.cleanup()
-
 conn.close()
+
+# Release the camera
+cap.release()
+cv2.destroyAllWindows()
