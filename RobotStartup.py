08c8cfe09257fab50af784a9cc1c8bddb6228ccb
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-04 20:38:01 -0400

    robot code update

--- RobotStartup.py
+++ RobotStartup.py
@@ -1,71 +1,63 @@
 import socket
 import threading
-import cv2
 import struct
 import pickle
+import picamera
+import io
 
-# Function to send camera frames to the client
-def send_frame(conn, frame):
-    frame_data = pickle.dumps(frame)
-    frame_size = struct.pack('!L', len(frame_data))
-    conn.sendall(frame_size + frame_data)
-
-# Function to handle client connections
 def handle_client(conn, addr):
     print("Client connected:", addr)
 
-    cap = cv2.VideoCapture(0)  # Use 0 for the first USB camera, 1 for the second, and so on
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  # Adjust the resolution as needed
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)  # Adjust the resolution as needed
 
-    try:
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            elif data == 'camera_frame':
-                ret, frame = cap.read()
-                if ret:
-                    # Send the camera frame to the client
-                    frame_data = pickle.dumps(frame)
+        try:
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
+                elif data == 'camera_frame':
+                    # Capture a frame from the camera
+                    stream = io.BytesIO()
+                    camera.capture(stream, format='jpeg')
+                    frame_data = stream.getvalue()
+
+                    # Send the size of the frame first
                     frame_size = len(frame_data)
                     conn.sendall(struct.pack('!L', frame_size))
+
+                    # Send the frame data
                     conn.sendall(frame_data)
-            else:
-                # Process the received command
-                print("Received command:", data)
-                # Implement actions based on the received command
-                # For example:
-                if data == 'forward':
-                    print("Moving forward")
-                elif data == 'backward':
-                    print("Moving backward")
-                elif data == 'left':
-                    print("Turning left")
-                elif data == 'right':
-                    print("Turning right")
+                else:
+                    # Process the received command
+                    print("Received command:", data)
+                    # Implement actions based on the received command
+                    # For example:
+                    if data == 'forward':
+                        print("Moving forward")
+                    elif data == 'backward':
+                        print("Moving backward")
+                    elif data == 'left':
+                        print("Turning left")
+                    elif data == 'right':
+                        print("Turning right")
 
-    except Exception as e:
-        print("Error handling client:", e)
+        except Exception as e:
+            print("Error handling client:", e)
 
-    finally:
-        cap.release()
-        conn.close()
-        print("Client disconnected:", addr)
-
-
-# Function to start the server
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('', 87)  # Use an empty string to listen on all available interfaces
-    server_socket.bind(server_address)
-    server_socket.listen(1)
-    print("Server is listening for incoming connections...")
-    while True:
-        conn, addr = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client, args=(conn, addr))
-        client_handler.daemon = True
-        client_handler.start()
+        finally:
+            conn.close()
+            print("Client disconnected:", addr)
 
 if __name__ == "__main__":
-    start_server()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('', 12345)
+    server_socket.bind(server_address)
+    server_socket.listen(5)
+
+    print("Waiting for connections...")
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.daemon = True
+        client_thread.start()
