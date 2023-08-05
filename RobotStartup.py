9edecccf62f42c93798be976c34c0fceddc69e87
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-04 20:51:58 -0400

    port + Ip chnage

--- RobotStartup.py
+++ RobotStartup.py
@@ -46,7 +46,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific IP and port
 host = '0.0.0.0'  # Use 0.0.0.0 to listen on all available interfaces
-port = 12345     # Choose a port number for the communication
+port = 87    # Choose a port number for the communication
 server_socket.bind((host, port))
 
 # Start listening for incoming connections
