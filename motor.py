0908e300df769e0b4563bf0f12f57404a5cddf88
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-08 20:48:54 -0400

    dual motor test

--- motor.py
+++ motor.py
@@ -1,32 +1,57 @@
-# Include the library files
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-# Include the motor control pins
-ENA = 17
-IN1 = 27
-IN2 = 22
-
+# Set GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(ENA,GPIO.OUT)
-GPIO.setup(IN1,GPIO.OUT)
-GPIO.setup(IN2,GPIO.OUT)
 
+# Define motor control pins
+motor1_pwm = 17  # Example GPIO pin for Motor 1 PWM
+motor1_in1 = 18  # Example GPIO pin for Motor 1 IN1
+motor1_in2 = 19  # Example GPIO pin for Motor 1 IN2
 
-def forward():
-    GPIO.output(ENA,GPIO.HIGH)
-    GPIO.output(IN1,GPIO.HIGH)
-    GPIO.output(IN2,GPIO.LOW)
+motor2_pwm = 27  # Example GPIO pin for Motor 2 PWM
+motor2_in1 = 20  # Example GPIO pin for Motor 2 IN1
+motor2_in2 = 21  # Example GPIO pin for Motor 2 IN2
 
+# Set up pins as output
+GPIO.setup(motor1_pwm, GPIO.OUT)
+GPIO.setup(motor1_in1, GPIO.OUT)
+GPIO.setup(motor1_in2, GPIO.OUT)
 
-def backward():
-    GPIO.output(ENA,GPIO.HIGH)
-    GPIO.output(IN1,GPIO.LOW)
-    GPIO.output(IN2,GPIO.HIGH)
+GPIO.setup(motor2_pwm, GPIO.OUT)
+GPIO.setup(motor2_in1, GPIO.OUT)
+GPIO.setup(motor2_in2, GPIO.OUT)
 
-while True:
-    forward()
-    sleep(1)
-    backward()
-    sleep(1)
+# Set up PWM
+motor1_pwm_obj = GPIO.PWM(motor1_pwm, 1000)  # Frequency: 1000 Hz
+motor2_pwm_obj = GPIO.PWM(motor2_pwm, 1000)
+
+# Start PWM with 0% duty cycle (motors off)
+motor1_pwm_obj.start(0)
+motor2_pwm_obj.start(0)
+
+# Function to control motor throttle
+def set_motor_speed(pwm_obj, in1, in2, speed):
+    if speed >= 0:
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+    else:
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+    pwm_obj.ChangeDutyCycle(abs(speed))
+
+try:
+    while True:
+        throttle_motor1 = float(input("Enter throttle for Motor 1 (-100 to 100): "))
+        throttle_motor2 = float(input("Enter throttle for Motor 2 (-100 to 100): "))
+        
+        set_motor_speed(motor1_pwm_obj, motor1_in1, motor1_in2, throttle_motor1)
+        set_motor_speed(motor2_pwm_obj, motor2_in1, motor2_in2, throttle_motor2)
+        
+except KeyboardInterrupt:
+    pass
+
+# Stop PWM and cleanup
+motor1_pwm_obj.stop()
+motor2_pwm_obj.stop()
+GPIO.cleanup()
