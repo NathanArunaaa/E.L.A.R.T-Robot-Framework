110c106f5b6280b7a95f61360053c9fd79552887
NathanArunaaa <nathanaruna07@gmail.com>
2023-08-10 23:43:55 -0400

    robot side rework t

--- Ctrl-Interface/Robot-Side.py
+++ Ctrl-Interface/Robot-Side.py
@@ -1,5 +1,3 @@
-#E.L.A.R.T Controller Reciever(robot side) By: Nathan Aruna
-
 import socket
 import cv2
 import threading
@@ -9,81 +7,100 @@ import time
 import os
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
-# Function to send frames to the client
-def send_frame(conn, frame):
-    frame_data = pickle.dumps(frame)
-    frame_size = struct.pack('!L', len(frame_data))
-    conn.sendall(frame_size + frame_data)
+# Function to handle sensor data
+def handle_sensor_connection(conn, addr):
+    try:
+        while True:
+            result = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True, text=True)
+            temperature_str = result.stdout.strip()
+            temperature = float(temperature_str.split('=')[1].replace("'C", ""))
+            temperature_data = f"Sensor data: {temperature:.2f} °C"
+            try:
+                conn.sendall(temperature_data.encode())
+            except (BrokenPipeError, ConnectionResetError):
+                print("Sensor Script: Client disconnected.")
+                break
+    except:
+        print("Sensor connection error:", addr)
+    finally:
+        conn.close()
 
-def sysReboot():
-    print('System Rebooting....')
-    time.sleep(5) 
-    os.system('sudo reboot')
-
-def sysShutdown():
-    print('System Shutdown....')
-    time.sleep(5)
-    os.system('sudo shutdown -h now')
+# Function to handle movement control
+def handle_controller_clinet(conn, addr):
     
-def motor_test():
+    def motor_test():
     # Set GPIO mode to BCM
-    GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
 
     # Define motor control pins for Motor 1
-    motor1_pwm = 17  # Example GPIO pin for Motor 1 PWM
-    motor1_in1 = 18  # Example GPIO pin for Motor 1 IN1
-    motor1_in2 = 19  # Example GPIO pin for Motor 1 IN2
+        motor1_pwm = 17  # Example GPIO pin for Motor 1 PWM
+        motor1_in1 = 18  # Example GPIO pin for Motor 1 IN1
+        motor1_in2 = 19  # Example GPIO pin for Motor 1 IN2
 
     # Define motor control pins for Motor 2
-    motor2_pwm = 27  # Example GPIO pin for Motor 2 PWM
-    motor2_in1 = 20  # Example GPIO pin for Motor 2 IN1
-    motor2_in2 = 12  # Example GPIO pin for Motor 2 IN2
+        motor2_pwm = 27  # Example GPIO pin for Motor 2 PWM
+        motor2_in1 = 20  # Example GPIO pin for Motor 2 IN1
+        motor2_in2 = 12  # Example GPIO pin for Motor 2 IN2
 
     # Set up pins as output for Motor 1
-    GPIO.setup(motor1_pwm, GPIO.OUT)
-    GPIO.setup(motor1_in1, GPIO.OUT)
-    GPIO.setup(motor1_in2, GPIO.OUT)
+        GPIO.setup(motor1_pwm, GPIO.OUT)
+        GPIO.setup(motor1_in1, GPIO.OUT)
+        GPIO.setup(motor1_in2, GPIO.OUT)
 
     # Set up pins as output for Motor 2
-    GPIO.setup(motor2_pwm, GPIO.OUT)
-    GPIO.setup(motor2_in1, GPIO.OUT)
-    GPIO.setup(motor2_in2, GPIO.OUT)
+        GPIO.setup(motor2_pwm, GPIO.OUT)
+        GPIO.setup(motor2_in1, GPIO.OUT)
+        GPIO.setup(motor2_in2, GPIO.OUT)
 
     # Set up PWM for both motors
-    motor1_pwm_obj = GPIO.PWM(motor1_pwm, 1000)  # Frequency: 1000 Hz
-    motor2_pwm_obj = GPIO.PWM(motor2_pwm, 1000)
-    motor1_pwm_obj.start(0)  # Start PWM with 0% duty cycle
-    motor2_pwm_obj.start(0)
-
+        motor1_pwm_obj = GPIO.PWM(motor1_pwm, 1000)  # Frequency: 1000 Hz
+        motor2_pwm_obj = GPIO.PWM(motor2_pwm, 1000)
+        motor1_pwm_obj.start(0)  # Start PWM with 0% duty cycle
+        motor2_pwm_obj.start(0)
+ 
     # Function to set motor speed
-    def set_motor_speed(pwm_obj, in1, in2, speed):
-        if speed >= 0:
-            GPIO.output(in1, GPIO.HIGH)
-            GPIO.output(in2, GPIO.LOW)
-        else:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.HIGH)
-        pwm_obj.ChangeDutyCycle(abs(speed))
+        def set_motor_speed(pwm_obj, in1, in2, speed):
+            if speed >= 0:
+                GPIO.output(in1, GPIO.HIGH)
+                GPIO.output(in2, GPIO.LOW)
+            else:
+                GPIO.output(in1, GPIO.LOW)
+                GPIO.output(in2, GPIO.HIGH)
+            pwm_obj.ChangeDutyCycle(abs(speed))
 
-    try:
-        speed = 100  # Set the speed as a percentage (-100 to 100)
-        print("at try stage")
-        set_motor_speed(motor1_pwm_obj, motor1_in1, motor1_in2, speed)
-        set_motor_speed(motor2_pwm_obj, motor2_in1, motor2_in2, -speed)
+        try:
+            speed = 100  # Set the speed as a percentage (-100 to 100)
+            print("at try stage")
+            set_motor_speed(motor1_pwm_obj, motor1_in1, motor1_in2, speed)
+            set_motor_speed(motor2_pwm_obj, motor2_in1, motor2_in2, -speed)
         
         # Allow motors to run for 5 seconds
-        time.sleep(5)
+            time.sleep(5)
         
-    finally:
+        finally:
         # Stop PWM and cleanup
-        motor1_pwm_obj.stop()
-        motor2_pwm_obj.stop()
-        GPIO.cleanup()
-    
-# Function to handle client connections
-def handle_client(conn, addr):
-    print("Client connected:", addr)
+            motor1_pwm_obj.stop()
+            motor2_pwm_obj.stop()
+            GPIO.cleanup()
+        
+    def send_frame(conn, frame):
+        frame_data = pickle.dumps(frame)
+        frame_size = struct.pack('!L', len(frame_data))
+        conn.sendall(frame_size + frame_data)
+
+    def sysReboot():
+        print('System Rebooting....')
+        time.sleep(5) 
+        os.system('sudo reboot')
+
+    def sysShutdown():
+        print('System Shutdown....')
+        time.sleep(5)
+        os.system('sudo shutdown -h now')
+        print("Client connected:", addr)
+        
     try:
         while True:
             data = conn.recv(1024).decode()
@@ -132,31 +149,31 @@ def handle_client(conn, addr):
     finally:
         conn.close()
         print("Client disconnected:", addr)
-
-# Create a socket object for the server
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to a specific IP and port
-host = '0.0.0.0'  # Use 0.0.0.0 to listen on all available interfaces
-port = 87    
-server_socket.bind((host, port))
-
-# Start listening for incoming connections
-server_socket.listen()
-
-print("Waiting for connections...")
-
+        
 camera = cv2.VideoCapture(0)  # Use 0 for the first camera device (change the index if needed)
 
-try:
-    while True:
-        conn, addr = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client, args=(conn, addr))
-        client_handler.daemon = True
-        client_handler.start()
-except KeyboardInterrupt:
-    print("Server stopped.")
 
-# Release the camera and close the server socket
-camera.release()
-server_socket.close()
+# Set up a socket server for sensor data
+sensor_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sensor_server_address = ('0.0.0.0', 86)  # Replace with your server's IP and port
+sensor_server_socket.bind(sensor_server_address)
+sensor_server_socket.listen()
+
+# Set up a socket server for movement control
+movement_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+movement_server_address = ('0.0.0.0', 87)  # Replace with your server's IP and port
+movement_server_socket.bind(movement_server_address)
+movement_server_socket.listen()
+
+print("Central Controller: Waiting for sensor connection...")
+while True:
+    sensor_conn, sensor_addr = sensor_server_socket.accept()
+    print("Central Controller: Sensor connected:", sensor_addr)
+    sensor_thread = threading.Thread(target=handle_sensor_connection, args=(sensor_conn, sensor_addr))
+    sensor_thread.start()
+
+    movement_conn, movement_addr = movement_server_socket.accept()
+    print("Central Controller: Movement connected:", movement_addr)
+    movement_thread = threading.Thread(target=handle_controller_clinet, args=(movement_conn, movement_addr))
+    movement_thread.start()
+
