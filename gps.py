cf142c03656c593f235ebbc0b69cd5bfd5fba2a8
NathanArunaaa <nathanaruna07@gmail.com>
2023-09-22 13:28:39 -0400

    gps code change

--- gps.py
+++ gps.py
@@ -1,60 +1,40 @@
-import serial               #import serial pacakge
-from time import sleep
-import webbrowser           #import package for opening link in browser
-import sys                  #import system package
+import serial
+import pynmea2  # Library to parse NMEA sentences
+import datetime
 
-def GPS_Info():
-    global NMEA_buff
-    global lat_in_degrees
-    global long_in_degrees
-    nmea_time = []
-    nmea_latitude = []
-    nmea_longitude = []
-    nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
-    nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
-    nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
-    
-    print("NMEA Time: ", nmea_time,'\n')
-    print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
-    
-    lat = float(nmea_latitude)                  #convert string into float for calculation
-    longi = float(nmea_longitude)               #convertr string into float for calculation
-    
-    lat_in_degrees = convert_to_degrees(lat)    #get latitude in degree decimal format
-    long_in_degrees = convert_to_degrees(longi) #get longitude in degree decimal format
-    
-#convert raw NMEA string into degree decimal format   
-def convert_to_degrees(raw_value):
-    decimal_value = raw_value/100.00
-    degrees = int(decimal_value)
-    mm_mmmm = (decimal_value - int(decimal_value))/0.6
-    position = degrees + mm_mmmm
-    position = "%.4f" %(position)
-    return position
-    
+# Define the serial port and baud rate
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
-
-gpgga_info = "$GPGGA,"
-ser = serial.Serial ("/dev/ttyS0")              #Open port with baud rate
-GPGGA_buffer = 0
-NMEA_buff = 0
-lat_in_degrees = 0
-long_in_degrees = 0
+# Create a file to log GPS data
+log_file = open("gps_log.txt", "a")
 
 try:
     while True:
-        received_data = (str)(ser.readline())                   #read NMEA string received
-        GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
-        if (GPGGA_data_available>0):
-            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
-            NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
-            GPS_Info()                                          #get time, latitude, longitude
- 
-            print("lat in degrees:", lat_in_degrees," long in degree: ", long_in_degrees, '\n')
-            map_link = 'http://maps.google.com/?q=' + lat_in_degrees + ',' + long_in_degrees    #create link to plot location on Google map
-            print("<<<<<<<<press ctrl+c to plot location on google maps>>>>>>\n")               #press ctrl+c to plot on map and exit 
-            print("------------------------------------------------------------\n")
-                        
+        # Read a line from the GPS module
+        sentence = ser.readline().decode('utf-8')
+        
+        # Check if the sentence is a valid NMEA sentence
+        if sentence.startswith('$GPGGA'):
+            try:
+                # Parse the NMEA sentence
+                msg = pynmea2.parse(sentence)
+                
+                # Extract GPS data
+                timestamp = datetime.datetime.combine(datetime.date.today(), msg.timestamp).strftime("%Y-%m-%d %H:%M:%S")
+                latitude = msg.latitude
+                longitude = msg.longitude
+                altitude = msg.altitude
+                
+                # Log the data to a file
+                log_entry = f"Time: {timestamp}, Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude} meters\n"
+                log_file.write(log_entry)
+                print(log_entry, end='')  # Print to console for debugging
+                
+            except pynmea2.ParseError as e:
+                print(f"Error parsing NMEA sentence: {str(e)}")
+            
+        # You can add more conditions to parse other NMEA sentence types if needed
+        
 except KeyboardInterrupt:
-    webbrowser.open(map_link)        #open current position information in google map
-    sys.exit(0)
+    log_file.close()
+    print("\nGPS logging stopped. Log file saved as 'gps_log.txt'")
