c147d6e0af3bf9383c65a3428e04ca43aaf7393d
NathanArunaaa <nathanaruna07@gmail.com>
2023-09-22 13:39:24 -0400

    gps remove log file

--- gps.py
+++ gps.py
@@ -1,40 +1,24 @@
 import serial
-import pynmea2  # Library to parse NMEA sentences
-import datetime
+import pynmea2
 
-# Define the serial port and baud rate
 ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
-
-# Create a file to log GPS data
-log_file = open("gps_log.txt", "a")
 
 try:
     while True:
-        # Read a line from the GPS module
         sentence = ser.readline().decode('utf-8')
         
-        # Check if the sentence is a valid NMEA sentence
         if sentence.startswith('$GPGGA'):
             try:
-                # Parse the NMEA sentence
                 msg = pynmea2.parse(sentence)
                 
-                # Extract GPS data
-                timestamp = datetime.datetime.combine(datetime.date.today(), msg.timestamp).strftime("%Y-%m-%d %H:%M:%S")
-                latitude = msg.latitude
-                longitude = msg.longitude
-                altitude = msg.altitude
-                
-                # Log the data to a file
-                log_entry = f"Time: {timestamp}, Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude} meters\n"
-                log_file.write(log_entry)
-                print(log_entry, end='')  # Print to console for debugging
-                
+                if msg.timestamp is not None:
+                    latitude = msg.latitude
+                    longitude = msg.longitude
+                    
+                    print(f"Latitude: {latitude}, Longitude: {longitude}")
+                    
             except pynmea2.ParseError as e:
                 print(f"Error parsing NMEA sentence: {str(e)}")
             
-        # You can add more conditions to parse other NMEA sentence types if needed
-        
 except KeyboardInterrupt:
-    log_file.close()
-    print("\nGPS logging stopped. Log file saved as 'gps_log.txt'")
+    print("\nGPS data printing stopped.")
