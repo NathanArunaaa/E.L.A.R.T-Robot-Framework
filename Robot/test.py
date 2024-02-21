605b6142fc289ca590a307655fb9eba8ed477840
NathanArunaaa <nathanaruna07@gmail.com>
2024-02-20 22:58:44 -0500

    address change

--- Robot/test.py
+++ Robot/test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Adjust port name as needed
+ser = serial.Serial('/dev/ttyACM0', 9600)  # Use the correct USB port address
 
 try:
     while True:
