a7c701601256814e6279f5d7ed65c6118ee7071c
NathanArunaaa <nathanaruna07@gmail.com>
2024-02-23 12:53:06 -0500

    test2

--- Robot/Robot-Side.py
+++ Robot/Robot-Side.py
@@ -387,9 +387,7 @@ def handle_sensor_connection(conn, addr):
             temperature_data = f"[CPU TEMP: {cpu_temperature:.2f} °C] [DS18B20 TEMP: {ds18b20_temperature:.2f} °C]" if ds18b20_temperature is not None else f"[CPU TEMP: {cpu_temperature:.2f} °C] [DS18B20 NOT FOUND]"
 
             # Combine with sensor values
-            if sensor_values:
-                for key, value in sensor_values.items():
-                    temperature_data += f" [{key}: {value}]"
+            
 
             # Send data to controller
             try:
